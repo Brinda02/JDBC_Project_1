87dc62d077370b89ee4bc68f656b42d91adab44b
brind <brind@BrindaHPE.lan>
2025-05-06 09:26:02 +0530

    commit

--- src/com/test/program_01.java
+++ src/com/test/program_01.java
@@ -3,8 +3,7 @@ package com.test;
 public class program_01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+          System.out.println("Hello World");
 	}
 
 }
