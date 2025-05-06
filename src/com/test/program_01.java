4a7fe8c254b3ceffb613e890cf9ee35962c39521
Brinda02 <102955977+Brinda02@users.noreply.github.com>
2025-05-06 09:31:56 +0530

    Update program_01.java

--- src/com/test/program_01.java
+++ src/com/test/program_01.java
@@ -1,9 +1,14 @@
 package com.test;
-
+import java.util.Scanner;
 public class program_01 {
 
 	public static void main(String[] args) {
           System.out.println("Hello World");
+		Scanner sc = new Scanner(System.in);
+         // Square of a number
+		System.out.print("Enter a number: ");
+		int n - s.nextInt();
+		System.out.println("Square of "+n+" is: "+(n*n)");
 	}
 
 }
